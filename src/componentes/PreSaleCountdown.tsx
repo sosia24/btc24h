738ad50dc4bc6c281c798a49834be25223ab6461
sosia24 <sosia24@proton.me>
2024-12-03 01:28:23 -0300

    New Commit

--- src/componentes/PreSaleCountdown.tsx
+++ src/componentes/PreSaleCountdown.tsx
@@ -32,29 +32,29 @@ const PreSaleCountdown = () => {
 
   return (
     <div className="bg-black bg-opacity-50 sm:p-8 p-4 rounded-lg shadow-2xl text-center mb-8">
-      <h3 className="text-3xl font-semibold text-white mb-6">Contagem Regressiva para o Fim da Pré-Venda</h3>
+      <h3 className="text-3xl font-semibold text-white mb-6">Countdown to the end of pre-sale</h3>
       <div className="flex flex-wrap justify-center gap-2 sm:gap-6 md:gap-8">
         {/* Cartões para dias */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-[16px]  text-[12px] font-bold">Dias</h4>
+          <h4 className="md:text-[16px]  text-[12px] font-bold">Days</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{days}</p>
         </div>
 
         {/* Cartões para horas */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-[16px] text-[12px] font-bold">Horas</h4>
+          <h4 className="md:text-[16px] text-[12px] font-bold">Hours</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{hours}</p>
         </div>
 
         {/* Cartões para minutos */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-[16px]  text-[12px] font-bold">Minutos</h4>
+          <h4 className="md:text-[16px]  text-[12px] font-bold">Minutes</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{minutes}</p>
         </div>
 
         {/* Cartões para segundos */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-green-500 via-green-700 to-green-900 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-[16px] text-[12px] font-bold">Segundos</h4>
+          <h4 className="md:text-[16px] text-[12px] font-bold">Seconds</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{seconds}</p>
         </div>
       </div>
