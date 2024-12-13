f7506249dc0d74bcc9f5074168e8c284b60a896e
sosia24 <sosia24@proton.me>
2024-12-13 00:01:22 -0300

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -181,7 +181,7 @@ async function hasQuote() {
       <div className="relative z-10 container mx-auto px-4 py-12">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-6xl font-extrabold mb-6 text-center text-white text-shadow-md">
-            Welcome to BTC24H Pre-Sale
+            Welcome to BTC24H
           </h1>
           <p className="text-2xl mb-8 text-center text-white text-shadow-md">
             Get the BTC24H token before it's too late!
@@ -192,7 +192,7 @@ async function hasQuote() {
           {/* Card de Venda */}
           <div className="bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 p-6 rounded-xl shadow-2xl transform hover:scale-105 transition-transform duration-300 max-w-md w-full">
             <h2 className="text-3xl font-semibold text-center text-white mb-4 tracking-wide text-shadow-md">
-              Buy BTC24H Token 
+              PreSale has ended!
             </h2>
 
             {/* Imagem do Token */}
@@ -205,71 +205,18 @@ async function hasQuote() {
             </div>
 
             <div className="mt-[15px] w-[100%] flex flex-col items-center justify-center text-white">
-            <p className="text-[20px] font-semibold">Quota Price</p>
-            <p className="mt-[10px] text-[18px] sm:text-[24px] text-center font-semibold">
-              {priceShare? priceShare : "..."}$
-            </p>
-            <p className="mt-[5px] text-[18px] sm:text-[24px] text-center font-semibold">
-              Shares: {totalSharesSold? totalSharesSold : "..."}
-            </p>
+            <p className="md:text-[24px] font-semibold text-[20px]">Results PreSale</p>
+            <p className="mt-[10px] text-[18px] sm:text-[24px] text-center font-semibold"></p>
+            <p className="mt-[10px] text-[16px] sm:text-[20px] text-center font-semibold">Shares Sold: 73</p>
+            <p className="m-[10px]">Each share receive: <span className="text-orange-500">643.83 BTC24h</span></p>
             
-            {/* New Section: Moedas Recebidas por Cota */}
-            <div className="mt-[20px] max-w-[98%] flex items-center justify-center bg-gradient-to-r from-[#ffb74d] to-[#ff7f50] py-[4px] px-[30px] rounded-xl flex-col">
-              <p className="text-gray-200  text-[12px] sm:text-[16px]">
-               Each quota receives 
-              </p>
-              <span className="flex flex-col text-white font-semibold text-[18px]">{Number(110000/totalSharesSold).toFixed(2)}</span>
-              <p>BTC24H</p>
-            </div>
-            <p className="text-gray-200 flex flex-col text-[12px] mt-[5px]">110 000 / {totalSharesSold}</p>
           </div>
 
             {/* Botão de Compra */}
 
-            {userHasShare?(
-            <p className="mt-[12px] sm:text-[18px] text-center text-[14px] justify-center m-auto items-center text-white">Congratulations, your quota is now guaranteed.</p>
-          ):(
-            <>
-            {usdtAllowance >= priceShare?(
-               <button
-               onClick={buyQuota}
-               className="w-full mb-[8px] bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-500 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
-               >
-               Buy Share
-                </button>
-            ):(
-              <>
-              <button
-              onClick={approveUsdt}
-              className="w-full mb-[8px] bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-500 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
-              >
-              Approve USDT
-              </button>
-              <button
-                className="w-full mb-[8px] bg-gray-500 cursor-not-allowed text-white font-bold py-2 px-4 rounded-md shadow-md"
-                >
-              Buy Share
-              </button>
-              </>
-            )}
-            </>
-          )}
 
             {/* Conectar Carteira */}
-            <div className="text-center">
-              {address ? (
-                <div className="text-lg font-semibold text-white text-shadow-md">
-                  Wallet Connected: {address.slice(0, 4) + "..." + address.slice(-6)}
-                </div>
-              ) : (
-                <button
-                  onClick={handleLogin}
-                  className="w-full bg-gradient-to-r from-yellow-500 to-yellow-600 hover:from-yellow-600 hover:to-yellow-500 text-lg font-bold py-2 px-4 rounded-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
-                >
-                  Connect Wallet
-                </button>
-              )}
-            </div>
+        
           </div>
         </div>
       </div>
--- src/componentes/PreSaleCountdown.tsx
+++ src/componentes/PreSaleCountdown.tsx
@@ -4,7 +4,7 @@ const PreSaleCountdown = () => {
   const [timeLeft, setTimeLeft] = useState<number>(0);
 
   useEffect(() => {
-    const targetDate = new Date('2024-12-13T00:00:00Z').getTime(); // Data de término da pré-venda
+    const targetDate = new Date('2024-12-16T00:00:00Z').getTime(); // Data de término da pré-venda
     const interval = setInterval(() => {
       const currentTime = new Date().getTime();
       const remainingTime = targetDate - currentTime;
@@ -32,29 +32,29 @@ const PreSaleCountdown = () => {
 
   return (
     <div className="bg-black bg-opacity-50 sm:p-8 p-4 rounded-lg shadow-2xl text-center mb-8">
-      <h3 className="text-3xl font-semibold text-white mb-6">Contagem Regressiva para o Fim da Pré-Venda</h3>
+      <h3 className="text-3xl font-semibold text-white mb-6">Cowntdown to starts contribution system</h3>
       <div className="flex flex-wrap justify-center gap-2 sm:gap-6 md:gap-8">
         {/* Cartões para dias */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-lg text-[12px] font-bold">Dias</h4>
+          <h4 className="md:text-lg text-[12px] font-bold">Days</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{days}</p>
         </div>
 
         {/* Cartões para horas */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-lg text-[12px] font-bold">Horas</h4>
+          <h4 className="md:text-lg text-[12px] font-bold">Hours</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{hours}</p>
         </div>
 
         {/* Cartões para minutos */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-blue-500 via-purple-500 to-pink-500 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-lg text-[12px] font-bold">Minutos</h4>
+          <h4 className="md:text-lg text-[12px] font-bold">Minutes</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{minutes}</p>
         </div>
 
         {/* Cartões para segundos */}
         <div className="bg-gradient-to-r sm:h-[100px] h-[60px] from-green-500 via-green-700 to-green-900 text-white sm:p-4 p-2 rounded-lg shadow-lg w-[70px] sm:w-[80px] md:w-[110px]">
-          <h4 className="md:text-lg text-[12px] font-bold">Segundos</h4>
+          <h4 className="md:text-lg text-[12px] font-bold">Seconds</h4>
           <p className="md:text-3xl text-[16px] font-extrabold">{seconds}</p>
         </div>
       </div>
