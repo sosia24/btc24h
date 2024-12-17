ed98c2f3473b91ff602dd6705fb7a183e3b874b4
sosia24 <sosia24@proton.me>
2024-12-17 11:49:54 -0300

    new commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,28 +53,57 @@ export default function RootLayout({
           >
             <div className="w-full h-full flex items-center flex-col pb-[160px]">
               <button className="max-w-[350px] w-[92%] bg-white p-6 rounded-2xl mt-[50px] text-center font-bold text-black text-[30px]">!!! STATEMENT !!!</button>
-                <div className="mt-[40px] rounded-2xl w-[60%] sm:w-[90%] flex-col bg-white p-10 text-black flex justify-center ">
-                <p className="text-[50px]">
-                Statement
-                </p>
-                <span className="font-bold">Important Announcement for Our Community</span><br></br>
+              <div className="mt-[40px] rounded-2xl w-full sm:w-[90%] lg:w-[60%] flex-col bg-white p-4 sm:p-6 lg:p-10 text-black flex justify-center">
+  <p className="text-2xl sm:text-xl lg:text-[50px] font-semibold">
+    Statement
+  </p>
+  <span className="font-bold text-lg sm:text-base">
+    Important Announcement for Our Community
+  </span>
+  <br />
 
-                Dear Community Members,<br></br>
+  <p className="text-lg sm:text-base">
+    Dear Community Members,
+  </p>
+  <br />
 
-                In response to the incident that occurred in the system on December 16, 2024, our team has made a strategic decision to further strengthen the security and trust in our platform.<br></br><br></br>
+  <p className="text-lg sm:text-base">
+    In response to the incident that occurred in the system on December 16, 2024, our team has made a strategic decision to further strengthen the security and trust in our platform.
+  </p>
+  <br /><br />
 
-                We have conducted the burn of <span className="font-bold">94,934.55 tokens</span> from the Genesis Wallet (Initial Liquidity Pool). This process has been documented and can be verified through the following Hash ID:<br></br><br></br>
-                <a href="https://polygonscan.com/tx/0xbe58635ccbfd89202dbae1df2ea69667a40202eb9b6286decf2654d4112410e9" className="text-blue-500">0xbe58635ccbfd89202dbae1df2ea69667a40202eb9b6286decf2654d4112410e9</a>
+  <p className="text-lg sm:text-base">
+    We have conducted the burn of <span className="font-bold">94,934.55 tokens</span> from the Genesis Wallet (Initial Liquidity Pool). This process has been documented and can be verified through the following Hash ID:
+  </p>
+  <br /><br />
+  <a href="https://polygonscan.com/tx/0xbe58635ccbfd89202dbae1df2ea69667a40202eb9b6286decf2654d4112410e9" className="text-blue-500 break-all">
+    0xbe58635ccbfd89202dbae1df2ea69667a40202eb9b6286decf2654d4112410e9
+  </a>
 
-                This action was taken with the goal of preventing future issues and protecting the interests of all members of our community.<br></br><br></br>
+  <p className="text-lg sm:text-base mt-4">
+    This action was taken with the goal of preventing future issues and protecting the interests of all members of our community.
+  </p>
+  <br /><br />
 
-                With this measure, we reaffirm our commitment to providing a platform that is increasingly secure, reliable, and transparent for all our users.<br/><br></br>
+  <p className="text-lg sm:text-base">
+    With this measure, we reaffirm our commitment to providing a platform that is increasingly secure, reliable, and transparent for all our users.
+  </p>
+  <br /><br />
 
-                We appreciate your understanding and continued trust in our work. We remain dedicated to continuously improving our technology and processes to meet your expectations.<br></br><br></br>
+  <p className="text-lg sm:text-base">
+    We appreciate your understanding and continued trust in our work. We remain dedicated to continuously improving our technology and processes to meet your expectations.
+  </p>
+  <br /><br />
 
-                Sincerely,<br></br>
-                <span className="font-bold">The Platform Development BTC24H Team.</span>
-                </div>
+  <p className="text-lg sm:text-base">
+    Sincerely,
+  </p>
+  <br />
+  <span className="font-bold text-lg sm:text-base">
+    The Platform Development BTC24H Team.
+  </span>
+</div>
+
 
             
               {children}
