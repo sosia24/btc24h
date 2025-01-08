03ef1fa09c5a9f1e0ba6ecff9e217fe8df942ba1
sosia24 <sosia24@proton.me>
2025-01-07 21:00:21 -0300

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,9 +97,15 @@ function HomeContent() {
         </Link>
         <Link
           className="p-2 w-[300px] text-center lg:mb-0 border-2 border-[#08ff65] rounded-3xl mt-[10px] text-white hover:border-[#08ff67d3]"
-          href="/whitepaper"
+          href="https://btc24h.site/whitepaper/"
         >
-          Presentation
+          WhitePaper
+        </Link>
+        <Link
+          className="p-2 w-[300px] text-center lg:mb-0 border-2 border-[#08ff65] rounded-3xl mt-[10px] text-white hover:border-[#08ff67d3]"
+          href="https://btc24h.site/"
+        >
+          Institutional Website
         </Link>
         {socio?(
           <Link
